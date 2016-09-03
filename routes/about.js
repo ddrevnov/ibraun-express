9265da5f9a457553b2c97491a53f252407c791af
ddrevnov <ddrievnov@mail.ru>
2016-09-03 22:42:52 +0300

    Moved 'fortune cookie' functionality into module.

--- routes/about.js
+++ routes/about.js
@@ -1,18 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
-var fortunes = [
-"Победи свои страхи, или они победят тебя.",
-"Рекам нужны истоки.",
-"Не бойся неведомого.",
-"Тебя ждет приятный сюрприз.",
-"Будь проще везде, где только можно.",
-];
+var fortune = require('../lib/fortune');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-  res.render('about', { fortune: randomFortune });
+  res.render('about', { fortune: fortune.getFortune() });
 });
 
 module.exports = router;
